2a8e4615db24da88f2acfddbbf1074b3e364156b
Birkehoj <anders.birkehoj@gmail.com>
2016-05-03 10:35:25 +0200

    Pmac cell with correction for fast noise

--- Pmac_cell2.m
+++ Pmac_cell2.m
@@ -15,6 +15,8 @@ classdef Pmac_cell2 < handle
         prev_occ_prob;
         last_entry_residue, last_exit_residue;
         last_exit_cnt, last_entry_cnt;
+        old_entry_residue, old_exit_residue;
+        old_exit_cnt, old_entry_cnt;
     end
     
     methods
@@ -30,6 +32,10 @@ classdef Pmac_cell2 < handle
             obj.last_exit_residue = 0;
             obj.last_exit_cnt = realmin('double');
             obj.last_entry_cnt = realmin('double');
+            obj.old_exit_residue = 0; 
+            obj.old_entry_residue = 0; 
+            obj.old_exit_cnt = realmin('double');
+            obj.old_entry_cnt = realmin('double');
         end
         
         function [] = update(obj,occ_prob)
@@ -40,17 +46,27 @@ classdef Pmac_cell2 < handle
                 if(obj.prev_occ_prob < 0.5)
                     obj.last_exit_residue = 0;
                     obj.last_exit_cnt = 0;
-                    obj.last_entry_residue = obj.last_entry_residue / obj.last_entry_cnt;
-                end
-                obj.last_exit_cnt = obj.last_exit_cnt + 1;
-                % start counting the exit max up
+                    old_entry_residue_avg = obj.old_entry_residue / obj.old_entry_cnt;
+                    current_entry_residue_avg = obj.last_entry_residue / obj.last_entry_cnt;                    
+                    if(current_entry_residue_avg > old_entry_residue_avg)
+                        obj.last_entry_residue = current_entry_residue_avg;
+                        obj.old_entry_residue = 0;
+                        obj.old_entry_cnt = 0;
+                    else
+                        obj.last_entry_residue = old_entry_residue_avg;
+                    end
+                end                          
+
                 obj.last_exit_residue = obj.last_exit_residue + occupied_prob;
+                obj.last_exit_cnt = obj.last_exit_cnt + 1;      
+                obj.old_exit_residue = obj.old_exit_residue + occupied_prob;
+                obj.old_exit_cnt = obj.old_exit_cnt + 1;
                 
-                % increment entry event
-                if(obj.last_entry_residue > 0.0)
+                if(obj.last_exit_residue > 0.0)
                     next_entry_cnt = min(occupied_prob,obj.last_entry_residue);
                     obj.entry = obj.entry + next_entry_cnt;
                     obj.last_entry_residue = obj.last_entry_residue - next_entry_cnt;
+                    obj.old_entry_residue = obj.old_entry_residue - next_entry_cnt;
                 end
             else
                 free_prob = (0.5 - occ_prob) * 2;
@@ -59,16 +75,27 @@ classdef Pmac_cell2 < handle
                 if(obj.prev_occ_prob > 0.5)
                     obj.last_entry_residue = 0;
                     obj.last_entry_cnt = 0;
-                    obj.last_exit_residue = obj.last_exit_residue / obj.last_exit_cnt;
-                end
-                obj.last_entry_cnt = obj.last_entry_cnt + 1;
+                    old_exit_residue_avg = obj.old_exit_residue / obj.old_exit_cnt;
+                    current_exit_residue_avg = obj.last_exit_residue / obj.last_exit_cnt;
+                    if(current_exit_residue_avg > old_exit_residue_avg)
+                        obj.last_exit_residue = current_exit_residue_avg;
+                        obj.old_exit_residue = 0;
+                        obj.old_exit_cnt = 0;
+                    else
+                        obj.last_exit_residue = old_exit_residue_avg;
+                    end
+                end                
                 
                 obj.last_entry_residue = obj.last_entry_residue + free_prob;
+                obj.last_entry_cnt = obj.last_entry_cnt + 1;
+                obj.old_entry_residue = obj.old_entry_residue + free_prob;
+                obj.old_entry_cnt = obj.old_entry_cnt + 1;
                 
                 if(obj.last_exit_residue > 0.0)
                     next_exit_cnt = min(free_prob,obj.last_exit_residue);
                     obj.exit = obj.exit + next_exit_cnt;
                     obj.last_exit_residue = obj.last_exit_residue - next_exit_cnt;
+                    obj.old_exit_residue = obj.old_exit_residue - next_exit_cnt;
                 end
             end
             obj.prev_occ_prob = occ_prob;
